01c7e8433103363a603c741d3f10bedfe5a11a45
isami <isamimitani@live.se>
2020-05-06 14:46:46 +0200

    added encryption of received auth data

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,18 @@ const jwt = require('jsonwebtoken')
 const axios = require('axios')
 const qs = require('qs')
 const engines = require('consolidate');
+const crypto = require('crypto');
+const propertiesReader = require('properties-reader');
+const properties = propertiesReader('./app.properties');
+
+const iv = new Buffer('0000000000000000');
+
+// method to encrypt text data with given key
+const encrypt = (data, key) => {
+    var decodeKey = crypto.createHash('sha256').update(key, 'utf-8').digest();
+    var cipher = crypto.createCipheriv('aes-256-cbc', decodeKey, iv);
+    return cipher.update(data, 'utf8', 'hex') + cipher.final('hex');
+};
 
 const app = express();
 app.engine('hbs', engines.handlebars);
@@ -27,9 +39,10 @@ app.post('/credentials', (request, response) => {
     generateToken(privateKey, client_id, response)
 });
 
+// method to generate JWT and call API to generate access token
 function generateToken(privateKey, client_id, res) {
-    const tokenUrl = 'https://b2b.revolut.com/api/1.0/auth/token' // production url
-    // const tokenUrl = 'https://sandbox-b2b.revolut.com/api/1.0/auth/token' // test url
+    // const tokenUrl = 'https://b2b.revolut.com/api/1.0/auth/token' // production url
+    const tokenUrl = 'https://sandbox-b2b.revolut.com/api/1.0/auth/token' // test url
     const issuer = 'revolut-test-516c1.web.app' // Issuer for JWT, should be derived from your redirect URL
     const aud = 'https://revolut.com' // Constant
 
@@ -38,7 +51,10 @@ function generateToken(privateKey, client_id, res) {
         "sub": client_id,
         "aud": aud
     }
+    // generates json web token with given data
     const token = jwt.sign(payload, privateKey, { algorithm: 'RS256', expiresIn: 60 * 60 });
+    
+    // calls Revolut API to generate access token
     axios({
         method: 'POST',
         url: tokenUrl,
@@ -53,9 +69,14 @@ function generateToken(privateKey, client_id, res) {
         })
         // eslint-disable-next-line promise/always-return
     }).then((result) => {
-        const jwt = { JWT: token }
-        const responseJson = Object.assign(result.data, jwt);
-        res.json(responseJson);
+        // encrypts received data
+        const key = properties.get('encryption.key').toString();
+        const object = {
+            access_token: encrypt(result.data.access_token, key),
+            refresh_token: encrypt(result.data.refresh_token, key),
+            JWT: encrypt(token, key),
+        }
+        res.json(object);
     }).catch(e => {
         console.dir(e)
         res.send(e)
